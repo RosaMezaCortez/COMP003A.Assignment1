efdf534e90df813bed6cfc4e1741205f048a2d3f
juan_ <juan_@BLINGBLING>
2023-01-20 21:50:58 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,46 @@
+﻿//this is a single-line comment
+/*this is a multi-line comment 
+ * 
+ * Author:      Rosa Meza 
+ * Course:      COMP-003A-L01
+ * Purpose:     Code for Assignment 
+ */
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me");
+            Console.WriteLine("1. i am 5 foot 11 inches tall");
+            Console.WriteLine("2. my birthday is february 23");
+            Console.WriteLine("3. i have curly hair");
+            Console.WriteLine("4. i like to draw");
+            Console.WriteLine("5. i have a nintendo switch");
+            Console.WriteLine("6. my favorite video game is legend of zelda breath of the wild");
+            Console.WriteLine("7. my favorite song is becca by the sukis");
+            Console.WriteLine("8. im a senior in high school");
+            Console.WriteLine("9. i worked at the ice rink in hanford");
+            Console.WriteLine("10. my favorite color is pink");
+            Console.WriteLine("11. my favorite show is mob psycho 100");
+            Console.WriteLine("12. i like to cook");
+            Console.WriteLine("13. i like stickers");
+            Console.WriteLine("14. i have 3 best friends");
+            Console.WriteLine("15. i broke my finger when i was 10");
+            Console.WriteLine("16. i like to take pictures");
+            Console.WriteLine("17. i like to read");
+            Console.WriteLine("18. i like to watch bluey");
+            Console.WriteLine("19. i like my friends");
+            Console.WriteLine("20. i like to collect plushies");
+            Console.WriteLine("21. i worked at a haunted house");
+            Console.WriteLine("22. my favorite food is sushi");
+            Console.WriteLine("23. my favorite drink at starbucks is magno dragon fruit with lemonade");
+            Console.WriteLine("24. my comfort foods are grilled cheese and soup");
+            Console.WriteLine("25. money stresses me out");
+            Console.WriteLine("26. my favorite book is i was born for this by alice oseman");
+            Console.WriteLine("27. i dont really like to paint");
+            Console.WriteLine("28. i want to go to a college for computer science");
+            Console.WriteLine("29. i like kandi bracelets");
+        }
+    }
+}
